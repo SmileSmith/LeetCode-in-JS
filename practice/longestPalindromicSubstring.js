7d282e6fdc436ff2d9188d9d95658b72f4376afe
Smile <longde_chen@163.com>
2018-04-13 09:16:08 +0800

    动态规划不是最优解，用中心展开的方式处理

--- practice/longestPalindromicSubstring.js
+++ practice/longestPalindromicSubstring.js
@@ -3,29 +3,29 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    var result = '';
-    var length = s.length;
-    var isParity = s.length % 2 !== 0;
-    var isPalindrome = function isPalindrome(i, j) {
-        console.log('--- ' + i + ' +++ ' + j + ' ---\n');
-        if (s[i] === s[j]) {
-            if(j - i === 2) return true;
-            return isPalindrome((i + 2), (j - 2));
-        } else {
-            return false;
-        }
+  var length = s.length;
+  var resultLength = 1;
+  var left = 0;
+  var right = 0;
+
+  var getPalindrome = function getPalindrome(i, j) {
+    if (s[i] !== s[j]) return;
+    if (resultLength < j - i + 1) {
+      left = i;
+      right = j;
+      resultLength = j - i +1;
     }
-    
-    for (var i = 0; i < length; i++) {
-        var j = isParity ? length - i - 1: length - i - 2;
-        isParity = !isParity;
-        if (isPalindrome(i, j)) {
-            if (result.length < j - i + 1) {
-                result = s.substring(i, j + 1);
-            }
-        } 
+    if (i > 0 && j < length) {
+      getPalindrome((i - 1), (j + 1));
     }
-    return result;
+  }
+
+  for (var m = 0; m < length; m++) {
+    getPalindrome(m, m + 2);
+    getPalindrome(m, m + 1);
+  }
+
+  return s.substring(left, right + 1);
 };
 
-console.log(longestPalindrome('babad'));
+console.log(longestPalindrome("rgczcpratwyqxaszbuwwcadruayhasynuxnakpmsyhxzlnxmdtsqqlmwnbxvmgvllafrpmlfuqpbhjddmhmbcgmlyeypkfpreddyencsdmgxysctpubvgeedhurvizgqxclhpfrvxggrowaynrtuwvvvwnqlowdihtrdzjffrgoeqivnprdnpvfjuhycpfydjcpfcnkpyujljiesmuxhtizzvwhvpqylvcirwqsmpptyhcqybstsfgjadicwzycswwmpluvzqdvnhkcofptqrzgjqtbvbdxylrylinspncrkxclykccbwridpqckstxdjawvziucrswpsfmisqiozworibeycuarcidbljslwbalcemgymnsxfziattdylrulwrybzztoxhevsdnvvljfzzrgcmagshucoalfiuapgzpqgjjgqsmcvtdsvehewrvtkeqwgmatqdpwlayjcxcavjmgpdyklrjcqvxjqbjucfubgmgpkfdxznkhcejscymuildfnuxwmuklntnyycdcscioimenaeohgpbcpogyifcsatfxeslstkjclauqmywacizyapxlgtcchlxkvygzeucwalhvhbwkvbceqajstxzzppcxoanhyfkgwaelsfdeeviqogjpresnoacegfeejyychabkhszcokdxpaqrprwfdahjqkfptwpeykgumyemgkccynxuvbdpjlrbgqtcqulxodurugofuwzudnhgxdrbbxtrvdnlodyhsifvyspejenpdckevzqrexplpcqtwtxlimfrsjumiygqeemhihcxyngsemcolrnlyhqlbqbcestadoxtrdvcgucntjnfavylip"));
