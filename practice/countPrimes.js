6788c875ee750ddeb716ff8bb00495cd54b7a8a0
Smile <longde_chen@163.com>
2018-04-24 11:09:11 +0800

    countPrimes补充其他算法

--- practice/countPrimes.js
+++ practice/countPrimes.js
@@ -74,11 +74,43 @@ var countPrimes_beta = function(n) {
 
 
 /**
- * 结合alpha中的质数Array，和beta中根号2
+ * 结合alpha中的质数Array，和beta中根号2。采用数字表排除的功能，例如：
+ * 
+ * 先定义一个长度为n的boolean数组
+ * 
+ * 排除所有x=2的倍数
+ * for (i = 2; i < n; i +=2) Arr[i] = false
+ * 排除所有x=3的倍数
+ * for (i = 3; i < n; i +=3) Arr[i] = false
+ * 排除所有x=4的倍数
+ * for (i = 4; i < n; i +=4) Arr[i] = false
+ * 
+ * 这里注意到，排除4的倍数不需要从4开始，因为4*2 和4*3，已经在2和3的循环中包括了，因此i可以从x的平方开始
  * 
  * 
- * 1. 从2到n遍历i
- * 2. 如果有n = p * q, 因为p <= q, 所以 p <= 根号n，所以遍历到 i * i < num 即可
- * 3. 先判断i是否在质数数组中，再进行取余运算
+ * 
  * 
  */
+
+ /**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    if (n <= 2) return 0;
+    var primesArr = new Array(n);
+    primesArr.fill(true);
+    primesArr[0] = false;
+    primesArr[1] = false;
+
+    for(var i = 2; i * i < n; i++) {
+        if(!primesArr[i]) continue;
+        for(var j = i * i; j < n; j += i) {
+            primesArr[j] = false;
+        }
+    }
+
+    return primesArr.reduce((sum, curr) => {
+        return sum + ( curr ? 1 : 0);
+    }, 0);
+};
